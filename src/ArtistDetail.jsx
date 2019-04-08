6a4e4a9c401759627a24ef2b0031ec514e8b2675
Ben Newman <benjaminjnewman@gmail.com>
2019-04-08 07:37:58 -0400

    fix various typos, extract list map and placeholder for readability, add comments

--- src/ArtistDetail.jsx
+++ src/ArtistDetail.jsx
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import  { TabContent, TabPane } from 'react-bootstrap';
 
 // usage: <ArtistDetail activeID={activeID} activeArtistData={activeArtistData} initial={initialDetailView} loading={this.state.loadingDetail} className='col-sm-8'/>
 // props: {activeArtist: string UUID, activeArtistData: Json object, initial: boolean, loading: boolean}
 
 function placeholder(){
-	return (<h3> 'Welcome to the Hollywood Artist Directory' </h3>
-		<p> Please click the name of an individual from the list on this page to see more detailed information </p>);
+	return (<Fragment><h3> 'Welcome to the Hollywood Artist Directory' </h3>
+		<p> Please click the name of an individual from the list on this page to see more detailed information </p></Fragment>);
 }
 
 function infoHtml(artist){
-	return (<h3> `${artist.firstName} ${artist.lastName}`</h3>
-		<img alt=`${artist.firstName} ${artist.lastName}` src={artist.imageURL} />
-		<p> `${artist.art}`</p>
-		<p> `${artist.dateOfBirth}` xx years old </p>
-		<p> `Born in ${artist.placeOfBirth}`
+	return (<Fragment><h3> {`${artist.firstName} ${artist.lastName}`}</h3>
+		<img alt={`${artist.firstName} ${artist.lastName}`} src={artist.imageURL} />
+		<p> {artist.art}</p>
+		<p> {`${artist.dateOfBirth} xx years old `}</p>
+		<p> {`Born in ${artist.placeOfBirth}`}</p></Fragment>)
 }
 
 class ArtistDetail extends Component {
 	render() {
+		let showPlaceholder = !this.props.loading && (this.props.activeID === '1')
 		return (<TabContent>
-	        <TabPane eventKey='first'>
-	          <h3> {this.props} </h3>
-	          <img ></img>
-	          <p> 'Artist Type' </p>
-	          <p> 'DOB , Age, BirthPlace' </p>
+	        <TabPane eventKey={this.props.activeID}>
+	          {showPlaceholder ? placeholder() : infoHtml(this.props.activeArtistData)}
 	        </TabPane>
 	      </TabContent>)
 	}
--- src/ArtistList.jsx
+++ src/ArtistList.jsx
@@ -4,20 +4,33 @@ import { Nav } from 'react-bootstrap'
 // usage: <ArtistList artists={this.state.artistsList} loading={this.state.loadingList} activeID={this.state.activeID} error={this.state.listError} className='col-sm-4'/>
 // props: {artistList: an array of objects, isLoading: boolean, activeID: string, listError: boolean
 
-// Data Schema for /artists - {"artistID":"827jshsg6736y34788478dh","firstName":"Quentin","lastName":"Tarantino","imageURL":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Quentin_Tarantino_by_Gage_Skidmore.jpg/896px-Quentin_Tarantino_by_Gage_Skidmore.jpg","art":"direction"}
+// Data response from /artists - {"artistID":"827jshsg6736y34788478dh","firstName":"Quentin","lastName":"Tarantino","imageURL":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Quentin_Tarantino_by_Gage_Skidmore.jpg/896px-Quentin_Tarantino_by_Gage_Skidmore.jpg","art":"direction"}
+
+function artistMap(artistList) {
+  if (typeof artistList !== 'undefined') {
+    artistList.map(function(artist) {
+      return(<Nav.Item>
+              <Nav.Link eventKey={artist.artistID}><img src={artist.imageURL} alt={`${artist.firstName} ${artist.lastName}`}/> {`${artist.firstName} ${artist.lastName}`} </Nav.Link>
+            </Nav.Item>)
+      }
+    )
+  } else {
+    console.log('artist list is undefined')
+  }
+}
+function placeholder() {
+  return(<Nav.Item> <Nav.Link disabled eventKey='1'> {"Loading"}</Nav.Link> </Nav.Item>)
+}
 
 class ArtistList extends Component {
   render() {
     let {isLoading, listError, artistList} = this.props
     // display placeholder while loading and in case of API error
     let showList = !isLoading && !listError
+    let shouldMap = showList && (typeof artistList !== 'undefined') && artistList !== null
     return (
       <Nav variant='pills' className='flex-column'>
-      {!!showList ? artistList.map(function(artist) {
-        return(<Nav.Item>
-          <Nav.Link eventKey={artist.artistID}><img src={artist.imageURL} alt=`{artist.firstName} + {artists.lastName}`/> `{artist.firstName} + {artists.lastName}` </Nav.Link>
-          </Nav.Item>)
-        }) : <Nav.Item> <Nav.Link eventKey='1'> 'Loading'</Nav.Link> </Nav.Item>
+      {(showList && shouldMap) ? artistMap(artistList) : placeholder()
       }
       </Nav>)
   }
